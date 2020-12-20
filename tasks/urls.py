7f746ca5002580ca83a42426cb625561b5a18627
inertiax <64903552+inertiax@users.noreply.github.com>
2020-12-21 00:02:37 +0300

    changes on the frontend

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.index, name="list"),
     path('update_task/<str:pk>/', views.updateTask, name='update_task'),
     path('delete/<str:pk>/', views.deleteTask, name='delete'),
-    path('', views.trial, name="trial")
 ]
